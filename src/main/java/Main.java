3c63bd520b59315c39065352fd7e35ccf0e05c34
XentelGD <160429411+XentelGD@users.noreply.github.com>
2024-09-08 19:18:36 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,11 @@ public class Main
     public static void main(String[] args)
     {
         App.window = new RenderWindow(new VideoMode(1000, 800), "MyWindow");
-        float x = 0, y = 0;
-
         while (App.window.isOpen()) {
             App.checkEvents();
-            //App.window.clear();
-            for (int i = 0; i < (int)(Math.random() * 300000); i++) {
-                System.out.println("werew");
-            }
+            App.window.clear();
+            
             // game logic
-            x += (float)(250 * 0.0004 * 1.5);
-            y += (float)(200 * 0.001 * 1.5);
-            Draw.rect(x, y, 5, 5, new Color(0, 25, 100));
 
             App.window.display();
         }
